8b075782bbcb6f12e935f8ef749b0bbb1bacc525
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-02-07 10:42:37 -0800

    Update deno example + deps to latest versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use deno_core::futures::FutureExt;
 use deno_core::{resolve_import, ModuleLoader};
+use deno_core::{ModuleLoadResponse, RequestedModuleType};
 
 use anyhow::bail;
 use deno_ast::MediaType;
 use deno_ast::ParseParams;
 use deno_ast::SourceTextInfo;
-use deno_core::ModuleCode;
+use deno_core::FastString;
 use deno_core::ModuleSource;
+use deno_core::ModuleSourceCode;
 use deno_core::ModuleType;
 use deno_core::{anyhow, error::generic_error};
 
@@ -19,12 +21,6 @@ impl Default for TypescriptModuleLoader {
         Self {
             http: reqwest::Client::new(),
         }
-    }
-}
-
-impl TypescriptModuleLoader {
-    pub fn new(http: reqwest::Client) -> Self {
-        Self { http }
     }
 }
 
@@ -43,15 +39,17 @@ impl ModuleLoader for TypescriptModuleLoader {
         module_specifier: &deno_core::ModuleSpecifier,
         _maybe_referrer: Option<&deno_core::ModuleSpecifier>,
         _is_dyn_import: bool,
-    ) -> std::pin::Pin<Box<deno_core::ModuleSourceFuture>> {
+        requested_module_type: RequestedModuleType,
+    ) -> ModuleLoadResponse {
         let module_specifier = module_specifier.clone();
         let http = self.http.clone();
-        async move {
+        let future = async move {
             let (code, module_type, media_type, should_transpile) = match module_specifier
                 .to_file_path()
             {
                 Ok(path) => {
                     let media_type = MediaType::from_path(&path);
+
                     let (module_type, should_transpile) = match media_type {
                         MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => {
                             (ModuleType::JavaScript, false)
@@ -68,6 +66,12 @@ impl ModuleLoader for TypescriptModuleLoader {
                         _ => bail!("Unknown extension {:?}", path.extension()),
                     };
 
+                    if module_type == ModuleType::Json
+                        && requested_module_type != RequestedModuleType::Json
+                    {
+                        return Err(generic_error("Attempted to load JSON module without specifying \"type\": \"json\" attribute in the import statement."));
+                    }
+
                     (
                         tokio::fs::read_to_string(&path).await?,
                         module_type,
@@ -75,19 +79,24 @@ impl ModuleLoader for TypescriptModuleLoader {
                         should_transpile,
                     )
                 }
+
                 Err(_) => {
                     if module_specifier.scheme() == "http" || module_specifier.scheme() == "https" {
                         let http_res = http.get(module_specifier.to_string()).send().await?;
+
                         if !http_res.status().is_success() {
                             bail!("Failed to fetch module: {module_specifier}");
                         }
+
                         let content_type = http_res
                             .headers()
                             .get("content-type")
                             .and_then(|ct| ct.to_str().ok())
                             .ok_or_else(|| generic_error("No content-type header"))?;
+
                         let media_type =
                             MediaType::from_content_type(&module_specifier, content_type);
+
                         let (module_type, should_transpile) = match media_type {
                             MediaType::JavaScript | MediaType::Mjs | MediaType::Cjs => {
                                 (ModuleType::JavaScript, false)
@@ -103,13 +112,22 @@ impl ModuleLoader for TypescriptModuleLoader {
                             MediaType::Json => (ModuleType::Json, false),
                             _ => bail!("Unknown content-type {:?}", content_type),
                         };
+
+                        if module_type == ModuleType::Json
+                            && requested_module_type != RequestedModuleType::Json
+                        {
+                            return Err(generic_error("Attempted to load JSON module without specifying \"type\": \"json\" attribute in the import statement."));
+                        }
+
                         let code = http_res.text().await?;
+
                         (code, module_type, media_type, should_transpile)
                     } else {
                         bail!("Unsupported module specifier: {}", module_specifier);
                     }
                 }
             };
+
             let code = if should_transpile {
                 let parsed = deno_ast::parse_module(ParseParams {
                     specifier: module_specifier.to_string(),
@@ -119,14 +137,22 @@ impl ModuleLoader for TypescriptModuleLoader {
                     scope_analysis: false,
                     maybe_syntax: None,
                 })?;
+
                 parsed.transpile(&Default::default())?.text.into_boxed_str()
             } else {
                 code.into_boxed_str()
             };
 
-            let module = ModuleSource::new(module_type, ModuleCode::Owned(code), &module_specifier);
+            let module = ModuleSource::new(
+                module_type,
+                ModuleSourceCode::String(FastString::Owned(code)),
+                &module_specifier,
+            );
+
             Ok(module)
         }
-        .boxed_local()
+        .boxed_local();
+
+        ModuleLoadResponse::Async(future)
     }
 }
